d39286ca7150a17011c40dfd2186ca4fab3c8e82
Daniele Ricci <daniele@casaricci.it>
2018-02-27 16:57:35 +0100

    nginx: enable gzip compression
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- updater.sh
+++ updater.sh
@@ -48,6 +48,12 @@ server {
     charset utf-8;
     root /home/pi/webui/app;
 
+    gzip on;
+    gzip_types text/plain text/javascript text/css application/javascript application/json;
+    gzip_min_length 256;
+	gzip_comp_level 6;
+	gzip_buffers 16 8k;
+
     location /api/ {
         proxy_pass  http://localhost:7475/;
     }
