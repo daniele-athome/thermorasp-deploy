88dda417baef6778b8c719e6549d46d13fa2d03b
Daniele Ricci <daniele@casaricci.it>
2018-12-26 21:19:30 +0100

    Update topic prefix
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- tools/publish-temp.sh
+++ tools/publish-temp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mosquitto_pub -t \
- home-assistant/thermorasp/sensor/$1/temperature \
+ homeassistant/thermorasp/sensor/$1/temperature \
  -r -m \
  "{\"unit\": \"celsius\", \"timestamp\": \"$(date +'%Y-%m-%dT%H:%M:%S.000000')\", \"value\": $2}"
