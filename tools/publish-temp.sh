b0fad437575ecf4e17921d6e06de3c9e33993ecb
Daniele Ricci <daniele@casaricci.it>
2018-12-27 00:43:48 +0100

    Add hard-coded validity manual sensor readings
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- tools/publish-temp.sh
+++ tools/publish-temp.sh
@@ -3,4 +3,4 @@
 mosquitto_pub -t \
  homeassistant/thermorasp/sensor/$1/temperature \
  -r -m \
- "{\"unit\": \"celsius\", \"timestamp\": \"$(date +'%Y-%m-%dT%H:%M:%S.000000')\", \"value\": $2}"
+ "{\"unit\": \"celsius\", \"timestamp\": \"$(date +'%Y-%m-%dT%H:%M:%S.000000')\", \"value\": $2, \"validity\": 900}"
