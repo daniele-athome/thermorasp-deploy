b24338264a97b70011366dac83486a475f688933
Daniele Ricci <daniele@casaricci.it>
2018-12-28 16:40:08 +0100

    Improve publish-temp
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- tools/publish-temp.sh
+++ tools/publish-temp.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+if [[ "$1" == "" ]] || [[ "$2" == "" ]]; then
+  echo "Usage: $0 sensor_id temperature [validity]"
+  exit 1
+fi
+
+VALIDITY=$3
+if [[ "${VALIDITY}" == "" ]]; then
+  VALIDITY=900
+fi
+
 mosquitto_pub -t \
  homeassistant/thermorasp/sensor/$1/temperature \
  -r -m \
- "{\"unit\": \"celsius\", \"timestamp\": \"$(date +'%Y-%m-%dT%H:%M:%S.000000')\", \"value\": $2, \"validity\": 900}"
+ "{\"unit\": \"celsius\", \"timestamp\": \"$(date +'%Y-%m-%dT%H:%M:%S.000000')\", \"value\": $2, \"validity\": ${VALIDITY}}"
